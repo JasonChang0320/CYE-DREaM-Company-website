2065a9922af02735ef9309ea4181dcaa790b65a5
joey <t1616joy@yahoo.com.tw>
2022-08-11 10:15:50 +0800

    refresh

--- about/views.py
+++ about/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def showtemplate(request):
-    return render(request, 'about.html')
+    return render(request, 'AboutUs.html')
--- home/views.py
+++ home/views.py
@@ -2,13 +2,18 @@ from django.shortcuts import render
 from django.http import JsonResponse
 import json
 import os
+from .models import HomePage
+from django.views.generic import ListView
 
 # Create your views here.
-def showtemplate(request):
-    return render(request, 'index.html')
+# def showtemplate(request):
+#     return render(request, 'HomePage.html')
 
-def show_google_map(request):
-    return render(request,"map.html")
+def showMapPage(request):
+    return render(request, 'MapPage.html')
+
+# def show_google_map(request):
+#     return render(request,"map.html")
 
 def grid_dataset(request):
     with open("statics/map.geojson") as f:
@@ -16,3 +21,7 @@ def grid_dataset(request):
 
     return JsonResponse(data)
 
+class HomePageContentView(ListView):
+    model = HomePage
+    template_name = 'HomePage.html'
+    context_object_name = 'Home_Content'
