9725d3152f19e5a58a5f278dbcfa0d5b1726570b
Jason <jason032089@gmail.com>
2022-08-27 18:30:45 +0800

    add english version form, url  random token

--- map/form.py
+++ map/form.py
@@ -16,3 +16,29 @@ class VisitorForm(ModelForm):
             "email":forms.EmailInput(attrs={"class":"form-control","id":"input_bar","placeholder":"abc@xxx.com"}),
             "job_title":forms.TextInput(attrs={"class":"form-control","id":"input_bar","placeholder":"Affiliation"})
         }
+class VisitorForm_EN(ModelForm):
+    class Meta:
+        model = Visitor_Info
+        fields = ("name","email","job_title")
+        labels ={
+            "name": "",
+            "email": "",
+            "job_title": ""
+        }
+        widgets ={
+            "name":forms.TextInput(attrs={"class":"form-control",
+                                            "id":"input_bar",
+                                            "placeholder":"Name",
+                                            "oninvalid":"this.setCustomValidity('Please enter your name')",
+                                            "oninput":"setCustomValidity('')"}),
+            "email":forms.EmailInput(attrs={"class":"form-control",
+                                            "id":"input_bar",
+                                            "placeholder":"abc@xxx.com",
+                                            "oninvalid":"this.setCustomValidity('Please enter valid email')",
+                                            "oninput":"this.setCustomValidity('')"}),
+            "job_title":forms.TextInput(attrs={"class":"form-control",
+                                                "id":"input_bar",
+                                                "placeholder":"Affiliation",
+                                                "oninvalid":"this.setCustomValidity('Please enter your Affiliation')",
+                                                "oninput":"this.setCustomValidity('')"})
+        }
--- map/views.py
+++ map/views.py
@@ -3,28 +3,49 @@ from django.shortcuts import render
 import json
 from django.http import JsonResponse,HttpResponseRedirect
 from .models import MapPage,MapPage_EN
-from .form import VisitorForm
+from .form import VisitorForm,VisitorForm_EN
+from django.utils.crypto import get_random_string
 
 # Create your views here.
 def showMapPage(request):
     submit=False
+    token_length=32
+    random_token=get_random_string(length=token_length)
     if request.method == "POST":
         form= VisitorForm(request.POST)
         if form.is_valid():
             form.save()
-            return HttpResponseRedirect("/MapPage?submit==True")
+            return HttpResponseRedirect(f"/MapPage?{random_token}")
     else:
         form=VisitorForm
-        if "submit" in request.GET:
+        key="100"
+        for key in request.GET.keys():
+            pass
+        if len(key)==token_length:
             submit=True
-        MapPage_content=MapPage.objects.all()
-    return render(request, 'MapPage.html',{"MapPage_content":MapPage_content,"form":form,"submit":submit})
+    MapPage_content=MapPage.objects.all()
+    return render(request, 'MapPage.html',{"MapPage_content":MapPage_content,"form":form,"submit":submit,"token":random_token})
 
 def showMapPage_EN(request):
+    submit=False
+    token_length=32
+    random_token=get_random_string(length=32)
+    if request.method == "POST":
+        form= VisitorForm_EN(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(f"/MapPage/en?{random_token}")
+    else:
+        form=VisitorForm_EN
+        key="100"
+        for key in request.GET.keys():
+            pass
+        if len(key)==token_length:
+            submit=True
     MapPage_content=MapPage.objects.all()
     MapPage_EN_content=MapPage_EN.objects.all()
     zip_MapPage=zip(MapPage_content,MapPage_EN_content)
-    return render(request, 'MapPage_EN.html',{"MapPage_content":zip_MapPage})
+    return render(request, 'MapPage_EN.html',{"MapPage_content":zip_MapPage,"form":form,"submit":submit,"token":random_token})
 
 def grid_dataset(request):
     with open("statics/map.geojson") as f:
