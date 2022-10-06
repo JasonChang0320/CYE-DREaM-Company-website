332e553f0ebc308e19625cc999810ddcf8c01232
Jason <jason032089@gmail.com>
2022-10-06 23:56:08 +0800

    add tax ID number in contact page

--- contact/admin.py
+++ contact/admin.py
@@ -4,8 +4,8 @@ from .models import Contact,Contact_EN
 # Register your models here.
 @admin.register(Contact)
 class Contact_Admin(admin.ModelAdmin):
-    list_display = ( 'title', 'phone',"email","address","office_hour")
+    list_display = ( 'title', 'phone',"email","address","office_hour","tax_ID_number")
 
 @admin.register(Contact_EN)
 class ContactEN_Admin(admin.ModelAdmin):
-    list_display = ( 'title', 'phone',"email","address","office_hour")
+    list_display = ( 'title', 'phone',"email","address","office_hour","tax_ID_number")
